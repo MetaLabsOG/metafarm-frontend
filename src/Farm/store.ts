83fa634907e9b6cec3f66dea715a9d9d21d672c8
Nibiri <niburimiy@gmail.com>
2022-05-26 18:25:22 +0400

    fix: :bug: fix imports

--- src/Farm/store.ts
+++ src/Farm/store.ts
@@ -18,6 +18,8 @@ import {
     ALGO_ASSET,
     fetchAlgoPrice,
     $pricedAssets,
+    $networkTime,
+    Contract,
 } from '../common/store';
 import { groupBy, values } from 'ramda';
 import { LPTokenInfo, DexProvider, makeDex } from '../providers/dexesProvider';
