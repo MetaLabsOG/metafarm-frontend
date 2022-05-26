9cd7be6292e44027e6581293335ebbe19057a503
Daria Yakovleva <dariyakovleva@gmail.com>
2022-05-26 19:29:57 +0400

    fix round metadao

--- src/MetaDAO/styled.ts
+++ src/MetaDAO/styled.ts
@@ -189,10 +189,10 @@ export const Info = styled.div`
 export const LegendValues = styled.div`
     position: absolute;
     left: 123%;
-    top: 28%;
+    top: 20%;
     @media (max-width: 640px) {
         left: 100%;
-        top: 50%;
+        top: 40%;
     }
 `;
 
