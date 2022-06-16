f9d4ed1cc2f7bed1280f44a3ba60b554ec89c3ce
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2022-06-16 15:55:18 +0100

    feat: :sparkles: Projected total rewards
    Moved projecting rewards in time into Effector logic before values aggregation

--- src/Farm/PoolList/Pool/Pool.tsx
+++ src/Farm/PoolList/Pool/Pool.tsx
@@ -1,16 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Status } from '../../../Status';
-import {
-    $networkTime,
-    queryTimeUpdate,
-    Contract,
-    FarmType,
-    AllDefined,
-    ContractState,
-    Amount,
-    Time,
-    hasLocalState,
-} from '../../../common/store';
+import { $networkTime, queryTimeUpdate, Contract, FarmType, hasLocalState } from '../../../common/store';
 import { useStore, useStoreMap } from 'effector-react';
 import { PoolState } from './types';
 import { PoolInfo } from './PoolInfo';
@@ -19,36 +9,6 @@ import { PoolContainer, PoolLoadingAnimation } from './styled';
 import { $farmLPTokens, $farmRewardTokens } from '../../store';
 import { $stakingTokens } from '../../../Stake/store';
 import logo from '../../../imgs/logo.png';
-import { min } from 'ramda';
-
-const BIG_NUM = BigInt('1000000000000000000');
-
-const recalculatedReward = (contractState: AllDefined<ContractState<FarmType>>, currentBlock: Time): Amount => {
-    const { endBlock, rewardPerBlock } = contractState.initial;
-    const { totalStaked, lastUpdateBlock, rewardPerTokenStored } = contractState.global;
-    const { staked, reward, rewardPerTokenPaid } = contractState.local;
-
-    if (lastUpdateBlock >= currentBlock || totalStaked === BigInt(0) || staked === BigInt(0)) {
-        return reward;
-    }
-
-    const lastBlockWithRewards = min(currentBlock, endBlock);
-    const rewardBlocksPassed = lastBlockWithRewards - lastUpdateBlock;
-    const rewardPerTokenStoredNew =
-        rewardPerTokenStored + (BigInt(rewardBlocksPassed) * rewardPerBlock * BIG_NUM) / totalStaked;
-
-    const rewardToPayNow = (staked * (rewardPerTokenStoredNew - rewardPerTokenPaid)) / BIG_NUM;
-    return reward + rewardToPayNow;
-};
-
-const projectState = (contractState: ContractState<FarmType>, currentBlock: Time): ContractState<FarmType> => {
-    if (!hasLocalState(contractState)) {
-        return contractState;
-    }
-
-    const projectedReward = recalculatedReward(contractState, currentBlock);
-    return { ...contractState, local: { ...contractState.local, reward: projectedReward } };
-};
 
 export const Pool = ({ type, contract }: { type: FarmType; contract: Contract<FarmType> }) => {
     const currentBlock = useStore($networkTime);
@@ -79,26 +39,24 @@ export const Pool = ({ type, contract }: { type: FarmType; contract: Contract<Fa
             ? PoolState.Finished
             : PoolState.Running;
 
-    const projectedState = projectState(contract.state, currentBlock);
-
     if (poolState === PoolState.Running || poolState === PoolState.Upcoming || poolState === PoolState.Finished) {
         return (
             <PoolContainer>
                 <div onClick={() => setIsOpen(!isOpen && contract.ctc)}>
                     <PoolInfo
                         isOpen={isOpen}
-                        contractState={projectedState}
+                        contractState={contract.state}
                         poolState={poolState}
                         lpTokenInfo={lpTokenInfo}
                         rewardTokenInfo={rewardTokenInfo}
                         currentBlock={currentBlock}
                     />
                 </div>
-                {contract.ctc !== null && hasLocalState(projectedState) && isOpen && (
+                {contract.ctc !== null && hasLocalState(contract.state) && isOpen && (
                     <PoolActions
                         poolState={poolState}
                         ctc={contract.ctc}
-                        contractState={projectedState}
+                        contractState={contract.state}
                         lpTokenInfo={lpTokenInfo}
                         rewardTokenInfo={rewardTokenInfo}
                         setIsZapModalOpen={setIsOpen}
