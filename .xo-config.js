b42cea278802b52d277706e88474e9e4122c159b
MetaB0y <metahosting@protonmail.com>
2022-08-07 09:28:09 +0800

    Add xo, another linter, just for my own use at the moment

---
+++ .xo-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    space: 'true',
+    prettier: 'true',
+    rules: {
+        // maybe good idea but don't want to bother
+        'unicorn/filename-case': 'off',
+        // tbh not sure how it works, it conflicts with TS for me
+        'import/extensions': 'off',
+        // We have something like custom implicit convention which doesn't match
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/ban-types': [
+            'error',
+            {
+                types: {
+                    // I think it's too much and we already have lots of nulls.
+                    // See https://github.com/sindresorhus/meta/discussions/7 for details btw.
+                    null: false,
+                },
+                extendDefaults: true,
+            },
+        ],
+    },
+    plugins: ['effector'],
+    extends: [
+        'plugin:effector/recommended',
+        'plugin:effector/scope',
+        'plugin:effector/react',
+        'plugin:effector/future',
+    ],
+};
