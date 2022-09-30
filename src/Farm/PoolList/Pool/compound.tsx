79dcdc658e4502e749fcbc131f713249f8315fff
Daria Yakovleva <dariyakovleva@gmail.com>
2022-09-30 14:24:07 +0200

    add stake compound

--- src/Farm/PoolList/Pool/compound.tsx
+++ src/Farm/PoolList/Pool/compound.tsx
@@ -8,13 +8,13 @@ import {
     Priced,
     refreshAccountInfo,
 } from '../../../common/store';
-import { fromSmallestUnits } from '../../../common/lib';
-import { QueryType, runTransactions, SLIPPAGE } from '../../../Swap/Swap';
+import { algoexplorerTxLink, fromSmallestUnits } from '../../../common/lib';
+import { getBestSwap, QueryType, runTransactions, SLIPPAGE } from '../../../Swap/Swap';
 import { Account, Contract, ViewVal } from '../../../types';
 import { logFarmActionData } from '../../../logEvent';
 import { notify } from '../../../Components/Notification';
 import { reach } from '../../../AppContext';
-import { convertAmountToUSD } from './utils';
+import { convertAmountToUSD, isLPTokenInfo } from './utils';
 
 export const isCompoundEnabled = (lpTokenInfo: LPTokenInfo, reward_asset_id: number) => {
     if (lpTokenInfo.poolDex !== 'T2' && lpTokenInfo.poolDex !== 'PT') {
@@ -23,81 +23,118 @@ export const isCompoundEnabled = (lpTokenInfo: LPTokenInfo, reward_asset_id: num
     return reward_asset_id === lpTokenInfo.asset1 || reward_asset_id === lpTokenInfo.asset2;
 };
 
-export const runCompound = async (
+const zapCompound = async (
     account: Account,
-    ctc: Contract,
     lpTokenInfo: LPTokenInfo,
     rewardAsset: Priced<Asset>,
-    claimRewards: Amount
+    amount: bigint,
+    extraAlgoAmount: bigint
 ) => {
-    const rewardAssetId = rewardAsset.id;
+    console.log('start compound zap');
     const asset1Id = lpTokenInfo.asset1;
     const asset2Id = lpTokenInfo.asset2;
+    const rewardAssetId = rewardAsset.id;
 
-    console.log('COMPOUND', asset1Id, asset2Id, rewardAssetId);
-    if (!isCompoundEnabled(lpTokenInfo, rewardAssetId)) {
-        notify('Different assets for compound.', 'error');
-        return;
-    }
-    if (convertAmountToUSD(rewardAsset, claimRewards) < 0.01) {
-        notify('The compound is available only if rewards are worth more than 1 cent.', 'error');
-        return;
-    }
     const firstAsset = asset1Id === rewardAssetId ? asset1Id : asset2Id;
     const secondAsset = asset1Id === rewardAssetId ? asset2Id : asset1Id;
 
-    // Is logging 0 ok? it's still useful to see how many rewards a person sees on the screen in logs right?
-    logFarmActionData(account, 'COMPOUND', 0, lpTokenInfo, rewardAsset);
+    const dex = makeDex(lpTokenInfo.poolDex);
+    const pool = await dex.getPoolByAssets(lpTokenInfo.asset1, lpTokenInfo.asset2);
+    const zapOp = await pool.getZap(rewardAsset, amount, SLIPPAGE);
+    const zapTxIds = await runTransactions(account, zapOp);
+
+    if (zapTxIds === null) {
+        throw new Error('Internal zap error!');
+    }
+
+    let microAlgoLpAmount = BigInt(0);
+    if (secondAsset === 0 && extraAlgoAmount > BigInt(0)) {
+        const algoZap = await pool.getZap(ALGO_ASSET, extraAlgoAmount, SLIPPAGE);
+        const algoZapResult = await runTransactions(account, algoZap);
+
+        if (algoZapResult === null) {
+            throw new Error('Internal algo zap error!');
+        }
+
+        microAlgoLpAmount = algoZap.mint.minimalLiquidityIssued;
+        console.log('Algo zap res lp amount', microAlgoLpAmount);
+    }
+
+    console.log('COMPOUND ZAP', zapOp, zapTxIds);
+
+    const accInfo = await fetchAccountInfoFx(account);
+    const balances = getBalancesFromAccountInfo(accInfo);
+    // If mint transaction passed, tinyman could not return less than that (since txs are deterministic).
+    // TODO: we should check
+    // const microLpAmount = zapOp.mint.minimalLiquidityIssued + microAlgoLpAmount;
+    const microLpAmount = balances[lpTokenInfo.id];
+    return microLpAmount;
+};
+
+const swapCompound = async (account: Account, firstAsset: Asset, secondAsset: Asset, microAmount: Amount) => {
+    const amount = fromSmallestUnits(firstAsset, microAmount);
+    const bestSwap = await getBestSwap(account, secondAsset.id.toString(), firstAsset.id.toString(), amount.toString());
+    if (!bestSwap) {
+        throw new Error('Best Swap Failed');
+    }
+    const res = await runTransactions(account, bestSwap, amount);
+    if (!res) {
+        throw new Error('Best Swap Failed');
+    }
+
+    return bestSwap.best.amountOut;
+};
+
+export const runCompound = async (
+    account: Account,
+    ctc: Contract,
+    stakedAsset: Priced<LPTokenInfo> | Priced<Asset>,
+    rewardAsset: Priced<Asset>,
+    claimRewards: Amount
+) => {
+    const compoundAmount = convertAmountToUSD(rewardAsset, claimRewards);
+    console.log('COMPOUND', stakedAsset, rewardAsset, compoundAmount);
+    if (isLPTokenInfo(stakedAsset) && !isCompoundEnabled(stakedAsset, rewardAsset.id)) {
+        notify('Different assets for compound.', 'error');
+        return;
+    }
+    if (compoundAmount < 0.01) {
+        notify('The compound is available only if rewards are worth more than 1 cent.', 'error');
+        return;
+    }
+
+    logFarmActionData(account, 'COMPOUND', compoundAmount, stakedAsset, rewardAsset);
 
     try {
-        console.log('start claim');
         const { result } = await (ctc.apis.claim as ViewVal)([]);
         const [claimedAmountBignum, claimedExtraAlgosBignum] = result;
         const claimedAmount = claimedAmountBignum.toBigInt();
         const claimedExtraAlgos = claimedExtraAlgosBignum.toBigInt();
-        const reward_amount = fromSmallestUnits(rewardAsset, claimedAmount);
-        console.log('CLAIMED', reward_amount, claimedAmount, claimedExtraAlgos);
 
-        console.log('start zap');
-        const dex = makeDex(lpTokenInfo.poolDex);
-        const pool = await dex.getPoolByAssets(lpTokenInfo.asset1, lpTokenInfo.asset2);
-        const zapOp = await pool.getZap(rewardAsset, claimedAmount, SLIPPAGE);
-        const zapTxIds = await runTransactions(account, zapOp);
-
-        if (zapTxIds === null) {
-            throw new Error('Internal zap error!');
-        }
-
-        let microAlgoLpAmount = BigInt(0);
-        if (secondAsset === 0 && claimedExtraAlgos > BigInt(0)) {
-            const algoZap = await pool.getZap(ALGO_ASSET, claimedExtraAlgos, SLIPPAGE);
-            const algoZapResult = await runTransactions(account, algoZap);
-
-            if (algoZapResult === null) {
-                throw new Error('Internal algo zap error!');
+        let stakeAmount = claimedAmount;
+        if (isLPTokenInfo(stakedAsset)) {
+            stakeAmount = await zapCompound(account, stakedAsset, rewardAsset, claimedAmount, claimedExtraAlgos);
+        } else {
+            if (stakedAsset.id !== rewardAsset.id) {
+                stakeAmount = await swapCompound(account, rewardAsset, stakedAsset, claimedAmount);
             }
-
-            microAlgoLpAmount = algoZap.mint.minimalLiquidityIssued;
-            console.log('Algo zap res lp amount', microAlgoLpAmount);
+            if (claimedExtraAlgos > BigInt(0)) {
+                const extraStakeAmount = (stakeAmount = await swapCompound(
+                    account,
+                    ALGO_ASSET,
+                    stakedAsset,
+                    claimedExtraAlgos
+                ));
+                stakeAmount += extraStakeAmount;
+            }
         }
 
-        const accInfo = await fetchAccountInfoFx(account);
-        const balances = getBalancesFromAccountInfo(accInfo);
-        console.log('COMPOUND ZAP', zapOp, zapTxIds);
-
-        // If mint transaction passed, tinyman could not return less than that (since txs are deterministic).
-        // TODO: we should check
-        // const microLpAmount = zapOp.mint.minimalLiquidityIssued + microAlgoLpAmount;
-        const microLpAmount = balances[lpTokenInfo.id];
-        console.log('start stake', fromSmallestUnits(lpTokenInfo, microLpAmount), microLpAmount);
-
-        await (ctc.apis.stake as ViewVal)([microLpAmount]);
-
+        await (ctc.apis.stake as ViewVal)([stakeAmount]);
         notify('Compound done!', 'success');
     } catch (error) {
         const error_message = error instanceof Error ? error.message : String(error);
         console.log(error_message);
-        logFarmActionData(account, 'COMPOUND ERROR', 0, lpTokenInfo, rewardAsset, error_message);
+        logFarmActionData(account, 'COMPOUND ERROR', compoundAmount, stakedAsset, rewardAsset, error_message);
         if (error_message.includes('underflow')) {
             notify('Not enough LP tokens.', 'error');
         } else if (error_message.includes('cancelled') || error_message.includes('The User has rejected')) {
