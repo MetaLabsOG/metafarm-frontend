067d4278b8b5f75dafcc31d4eea4083a39085d45
wackloner <wackloner@gmail.com>
2024-06-05 07:02:05 +0800

    update build script

--- install_prod.sh
+++ install_prod.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -e
+
+echo "Installing Node.js 18.12.0"
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+
+node -v
+npm -v
+
 corepack enable
 
 corepack prepare yarn@4.2.2 --activate
 
 yarn install
+
+yarn build
